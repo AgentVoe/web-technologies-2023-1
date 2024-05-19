b10b3c42a3b0d7e2b598c4dc4526c1aba7ffad2f
Voe <mr.subiter@mail.ru>
2024-05-19 16:29:30 +0500

    lesson14

---
+++ catalogue.js
@@ -0,0 +1,70 @@
+import { Catalogue} from "./src/components/catalogue.js"
+
+const renderPostItem = item => `
+    <a  
+        href="posts.html?id=${item.id}"
+        class="post-item"
+    >
+        <span class="post-item__title">
+            ${item.title}
+        </span>
+
+        <span class="post-item__body">
+            ${item.body}
+        </span>
+    </a>
+`
+
+const getPostItems = async ({ limit, page }) => {
+    try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+        const total = +res.headers.get('x-total-count');
+        const items = await res.json();
+        return { items, total }
+    }
+    catch(err) {
+        console.log(err);
+    }
+};
+
+const renderPhotoItem = item => `
+    <a  
+        href="photos/${item.id}"
+        class="photo-item"
+    >
+        <span class="photo-item__title">
+            ${item.title}
+        </span>
+
+        <img 
+            src=${item.url}
+            class="photo-item__image"
+        >
+    </a>
+`
+
+const getPhotoItems = async ({ limit, page }) => {
+    try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`);
+        const total = +res.headers.get('x-total-count');
+        const items = await res.json();
+        return { items, total }
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+const init = () => {
+    const catalog = document.getElementById('catalog')
+    new Catalogue(catalog, { 
+        renderItem: renderPostItem,
+        getItems: getPostItems
+     }).init()
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init)
+} else {
+    init()
+}
